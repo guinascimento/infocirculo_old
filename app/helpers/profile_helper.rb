0a75ab085f6e796eab481551713fc483446c30e5
Guilherme Nascimento <guilherme.ruby@gmail.com>
2009-07-08 14:18:34 -0300

    refactoring personal informations page

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,14 +2,14 @@ module ProfileHelper
 
   def full_im
     case @profile.im_type
-    when :MSN
-      @profile.im + " (MSN)"
-    when :AIM
-      @profile.im + " (AIM)"
-    when :GTalk
-      @profile.im + " (GTalk)"
-    when :Skype
-      @profile.im + " (Skype)"
+      when :MSN
+        @profile.im + " (MSN)"
+      when :AIM
+        @profile.im + " (AIM)"
+      when :GTalk
+        @profile.im + " (GTalk)"
+      when :Skype
+        @profile.im + " (Skype)"
     end
   end
 
