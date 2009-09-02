2d1d70eaf8b40e7d8e66d8f17955167a5312cd6a
Guilherme Nascimento <guilherme.ruby@gmail.com>
2009-09-02 14:44:12 -0300

    lot of improvments on code

--- test/unit/helpers/personal_informations_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PersonalInformationsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/public_profile_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PublicProfileHelperTest < ActionView::TestCase
-end
